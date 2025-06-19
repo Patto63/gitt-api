aa164611dea44bf4b19579edf1e13fa1c0635a69
Patricio Salazar <psalazar1423@uta.edu.ec>
2025-06-19 08:18:30 -0500

    test 0.0.0.0.1

--- drizzle/data/conditions.ts
+++ drizzle/data/conditions.ts
@@ -3,7 +3,7 @@ import { condition } from 'drizzle/schema/tables/inventory/condition'
 export const conditionsSeed: (typeof condition.$inferInsert)[] = [
   {
     name: 'Bueno',
-    description: 'Bueno',
+    description: 'Bueno ',
     requiresMaintenance: false,
   },
   {
